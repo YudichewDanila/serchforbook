e6ee44433052fa35c4dde4383dffda6d1ad50e7f
Danill <117526746+YudichewDanila@users.noreply.github.com>
2023-09-07 14:03:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={ <MainPage />}></Route>
       <Route path="/book/:id" element={<BookPage />}></Route>
+      <Route path="*" element={<MainPage />} />
     </Routes> 
     </main>
   )
